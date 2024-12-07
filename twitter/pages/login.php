6e24e8750e187f494531fe628049efb3ac2f5be9
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-12-07 17:55:03 -0400

    le

---
+++ twitter/pages/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../styles/registration.css">
+</head>
+<div class="page">
+<nav>
+<?php include("../components/nav.php");?>    
+</nav>
+<body>
+<div class="container">
+    <h2>Sign In</h2>
+    <form id="loginForm">
+        <label for="username">Username or Email</label>
+        <input type="text" id="username" name="username" required>
+        
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+        
+        <button id="login" type="submit">Sign In</button>
+    </form>
+    <div class="message">
+    </div>
+</div>
+</body>
+<footer>
+<?php include("../components/footer.php");?>    
+</footer>
+</div>
+</html>
+
--- twitter/pages/registration.php
+++ twitter/pages/registration.php
@@ -1,6 +1,3 @@
-<?php
-include("../database.php");
-?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
 $(document).ready(function(){
@@ -19,8 +16,13 @@ $(document).ready(function(){
 <head>
     <link rel="stylesheet" href="../styles/registration.css">
 </head>
+<div class="page">
+<nav>
+<?php include("../components/nav.php");?>    
+</nav>
 <body>
 <div class="container">
+    <h2>Registration</h2>
  <form id="registerForm">
  <label for="username">Username</label>
  <input type="text" id="username" name="username" required>
@@ -34,5 +36,9 @@ $(document).ready(function(){
 </div>
 </div>
 </body>
+<footer>
+<?php include("../components/footer.php");?>    
+</footer>
+</div>
 </html>
 
